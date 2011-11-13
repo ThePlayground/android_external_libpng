79f8bdc6d5d8273ebad0e175b0b97844492711a9
Chao Yang <chao.yang@linaro.org>
2011-11-13 13:24:30 +0000

    libpng: integrate libpng 1.5.7 beta 01
    Integrate libpng 1.5.7 beta 01 so as to integrate libpng NEON
    optimisation.
    
    Change-Id: I78b738ce5fbce1426a9382885ad57d3d685f2249

--- Android.mk
+++ Android.mk
@@ -1,12 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 
-# We need to build this for both the device (as a shared library)
+# We need to build this for both the device (as a static library)
 # and the host (as a static library for tools to use).
 
 common_SRC_FILES := \
 	png.c \
 	pngerror.c \
-	pnggccrd.c \
 	pngget.c \
 	pngmem.c \
 	pngpread.c \
@@ -16,13 +15,12 @@ common_SRC_FILES := \
 	pngrutil.c \
 	pngset.c \
 	pngtrans.c \
-	pngvcrd.c \
 	pngwio.c \
 	pngwrite.c \
 	pngwtran.c \
 	pngwutil.c
 
-common_CFLAGS := ## -fomit-frame-pointer
+common_CFLAGS := -DPNG_USER_CONFIG
 
 common_C_INCLUDES += \
 
@@ -51,12 +49,14 @@ include $(BUILD_HOST_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
+LOCAL_SRC_FILES := $(common_SRC_FILES) \
+                   arm/filter_neon.S
+LOCAL_CFLAGS += $(common_CFLAGS) \
+                 -DPNG_ARM_NEON -DPNG_ALIGNED_MEMORY_SUPPORTED
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
 	external/zlib
 LOCAL_SHARED_LIBRARIES := \
-	libz
+	libz libm
 
 LOCAL_MODULE:= libpng
 
@@ -66,3 +66,62 @@ LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
 include $(BUILD_STATIC_LIBRARY)
 
 
+# Test Program pngvalid
+# =====================================================
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= pngvalid.c
+
+LOCAL_STATIC_LIBRARIES := libpng libz
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+	external/zlib
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLE)
+
+LOCAL_MODULE_TAGS := debug
+
+LOCAL_MODULE := pngvalid
+
+include $(BUILD_EXECUTABLE)
+
+# Test Program pngtest
+# =====================================================
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= pngtest.c
+
+LOCAL_STATIC_LIBRARIES := libpng libz
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+	external/zlib
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLE)
+
+LOCAL_MODULE_TAGS := debug
+
+LOCAL_MODULE := pngtest
+
+include $(BUILD_EXECUTABLE)
+
+# Test Program pngtest for the host
+# =====================================================
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= pngtest.c
+
+LOCAL_STATIC_LIBRARIES := libpng libz
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+	external/zlib
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLE)
+
+LOCAL_MODULE_TAGS := debug
+
+LOCAL_MODULE := pngtest
+
+include $(BUILD_HOST_EXECUTABLE)
